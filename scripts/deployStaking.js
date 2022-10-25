82af2ec62b7e99b10662f881e29908f73a864f6c
WongJapan <kulomilyarder@gmail.com>
2022-10-26 04:35:46 +0700

    checkpoint

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -4,8 +4,8 @@ const {REWARD_WALLET, TREASURY} = require("./constants/address");
 
 const stakeToken = "0x8655e717fA4157e1099F61bA261c2Cd7b121d661";
 const rewardToken = "0x8655e717fA4157e1099F61bA261c2Cd7b121d661";
-const rewardN = "100";
-const rewardD = "1000000";
+const rate = "5";
+const locktime = "1000000";
 
 async function main() {
   const ArborsStakingWithFixedLockTime = await ethers.getContractFactory("ArborsStakingWithFixedLockTime");
